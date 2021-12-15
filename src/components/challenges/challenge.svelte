a943ec2b5062cfb7826fa80e4a4b7ffed4962855
Hans Hofner <hhofner@gmail.com>
2021-12-15 22:36:02 +0900

    challenge: move to new file and stylistic changes

---
+++ src/components/challenges/challenge.svelte
@@ -0,0 +1,46 @@
+<script>
+	export let data;
+
+	import { Button } from "svelte-chota";
+
+</script>
+
+<div class='challenge'>
+	<div class='preview' >
+		<div>
+			<label>{data.name}</label>
+			<progress max={data.target} value={data.progress}></progress>
+
+			<small class='text-grey'> Deadline: {data.endDate} </small>
+
+		</div>
+
+		<span>movement graphs</span>
+		<div>
+					<Button primary>record</Button>
+					<!--        TODO: Make these "owner" only-->
+					<Button>edit</Button>
+					<Button error>delete</Button>
+					</div>
+	</div>
+</div>
+
+<style>
+    .preview {
+				padding: 1rem;
+        display: flex;
+				flex-wrap: wrap;
+        justify-content: space-between;
+        margin-bottom: 2em;
+
+				border-radius: 4px;
+				border: 1px solid transparent;
+    }
+		.preview:hover {
+        border-radius: 4px;
+        border: 1px solid var(--color-lightGrey);
+		}
+    small {
+        display: block;
+    }
+</style>
