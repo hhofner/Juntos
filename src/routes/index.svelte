03321bd34b0da054e3e6fe0bceb02e6cbf89f1cf
Hans Hofner <hhofner@gmail.com>
2021-12-16 10:13:59 +0900

    idea: try small team cards with icon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
-import { teams } from "$lib/stores.ts";
-import Section from "../components/section.svelte";
-import Challenge from "../components/challenges/challenge.svelte";
-import { Card } from "svelte-chota";
+  import { teams } from "$lib/stores.ts";
+  import Section from "../components/section.svelte";
+  import Challenge from "../components/challenges/challenge.svelte";
+  import { Card } from "svelte-chota";
 
-import tempData from "$lib/temp.json";
+  import tempData from "$lib/temp.json";
+  import { fly } from 'svelte/transition';
+  let visible = false;
 </script>
 
 <Section title="Your Challenges">
@@ -13,21 +15,40 @@ import tempData from "$lib/temp.json";
   {/each}
 </Section>
 
-<Section title="Your Teams" folded='true'>
+<Section title="Your Teams">
   <div class='teams'>
     {#each $teams as team}
     <Card>
-      <div><img src='https://icongr.am/material/account-group-outline.svg?size=66&color=currentColor'/></div>
+      <img
+        src='https://icongr.am/material/account-group-outline.svg?size=66&color=currentColor'
+        alt='Team'
+      />
       {team}
     </Card>
     {/each}
   </div>
 </Section>
 <Section title="Your Trophies" folded='true'/>
+<button on:click={() => visible=!visible}>open</button>
+{#if visible}
+  <div class='test-modal' transition:fly="{{ y: 100, duration: 200 }}">
+    tets
+  </div>
+{/if}
+
 
 <style>
   .teams {
     display: flex;
     gap: 0.5rem;
+  }
+  .test-modal {
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    background: blue;
+  }
+  img {
+    display: block;
   }
 </style>
